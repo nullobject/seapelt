b0ac53a8afbe41b0735098b58a31c508cf0a6a8c
Josh Bassett <josh.bassett@gmail.com>
2018-09-03 19:28:38 +1000

    Add social icons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,11 +3,20 @@ import Link from 'gatsby-link'
 
 import styles from './styles.module.scss'
 
+const FACEBOOK_URL = "https://facebook.com/seapeltmusic"
+const SOUNDCLOUD_URL = "https://soundcloud.com/seapelt"
+const TWITTER_URL = "https://twitter.com/seapelt"
+
 const Header = ({title}) => (
   <header className={styles.header}>
-    <h1>
+    <h2>
       <Link to="/">{title}</Link>
-    </h1>
+    </h2>
+    <nav className={styles.social}>
+      <a href={TWITTER_URL}><span className='icon-twitter' /></a>
+      <a href={FACEBOOK_URL}><span className='icon-facebook' /></a>
+      <a href={SOUNDCLOUD_URL}><span className='icon-soundcloud' /></a>
+    </nav>
   </header>
 )
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>SEAPELT</h1>
+    <h2>Releases</h2>
+    <p>Lorem ipsum dolor amet offal ennui man bun, franzen gluten-free jean shorts taxidermy waistcoat 90's. Kinfolk affogato photo booth, fingerstache live-edge man braid hella. 3 wolf moon plaid retro taxidermy cloud bread pabst portland keffiyeh knausgaard offal. Deep v slow-carb kale chips skateboard heirloom. Tofu enamel pin meggings mumblecore, viral pok pok af gluten-free plaid gochujang chambray keffiyeh prism marfa. Tumeric you probably haven't heard of them VHS, artisan marfa tattooed sustainable banh mi trust fund coloring book biodiesel whatever.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
