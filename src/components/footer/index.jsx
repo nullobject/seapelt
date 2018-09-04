5eb4b9edcf3a959f780060b03910964711f4c6e0
Josh Bassett <josh.bassett@gmail.com>
2018-09-04 22:14:41 +1000

    Remove social component

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import {OutboundLink} from 'gatsby-plugin-google-analytics'
 
-import Social from '../social'
 import styles from './styles.module.scss'
+
+const FACEBOOK_URL = 'https://facebook.com/seapeltmusic'
+const SOUNDCLOUD_URL = 'https://soundcloud.com/seapelt'
+const TWITTER_URL = 'https://twitter.com/seapelt'
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <Social />
+    <nav>
+      <OutboundLink href={TWITTER_URL}><span className='icon-twitter' /></OutboundLink>
+      <OutboundLink href={FACEBOOK_URL}><span className='icon-facebook' /></OutboundLink>
+      <OutboundLink href={SOUNDCLOUD_URL}><span className='icon-soundcloud' /></OutboundLink>
+    </nav>
   </footer>
 )
 
