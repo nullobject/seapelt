a6a5091c8ce2da1dbb89b08ca2b169da4e3c07a8
Josh Bassett <josh.bassett@gmail.com>
2018-09-04 12:49:50 +1000

    Change index page title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>SEAPELT</h1>
+    <h1>Seapelt</h1>
     <p>Seapelt is the moniker of Byron Bay down-tempo electronica artist Josh Bassett. His music explores the lines between digital and analog, a space where the human signal blurs with the natural environment.</p>
     <p>Incorporating elements of IDM with minimised electronica, modern classical, and ambient &mdash; his tracks evoke a sense of nostalgia for the lost sounds of decades past.</p>
     <Link to='/page-2/'>Go to page 2</Link>
