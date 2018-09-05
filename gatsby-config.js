96085c42a10fa4d98fe4d16f2900ba717edb575e
Josh Bassett <josh.bassett@gmail.com>
2018-09-05 17:48:54 +1000

    Add projects to data file

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,8 +25,15 @@ module.exports = {
         anonymize: true,
         respectDNT: true
       }
+    }, {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`
+      }
     },
     'gatsby-plugin-offline',
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    `gatsby-transformer-json`
   ]
 }
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
+import {graphql} from 'gatsby'
 
 import Layout from '../components/layout'
 
-const ProjectsPage = () => (
-  <Layout>
-    <p>Sydney Fringe Festival, 2018</p>
-    <p>Electrofringe Festival, 2018</p>
-    <p>Beat Collective, 2018</p>
-  </Layout>
+const Project = ({project: {date, title, location}}) => (
+  <li>
+    <time dateTime='{date}'>{date}</time>
+    <span>{title}</span> @ <span>{location}</span>
+  </li>
 )
 
+const ProjectsPage = ({data: {allProjectsJson: {edges}}}) => {
+  const projects = edges.map(edge => <Project key={edge.node.id} project={edge.node} />)
+  return (
+    <Layout>
+      <ul>{projects}</ul>
+    </Layout>
+  )
+}
+
 export default ProjectsPage
+
+export const pageQuery = graphql`
+  query {
+    allProjectsJson {
+      edges {
+        node {
+          id
+          date(formatString: "MMMM DD, YYYY")
+          title
+          location
+        }
+      }
+    }
+  }
+`
