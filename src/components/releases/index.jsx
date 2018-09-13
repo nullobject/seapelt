d0227f7d7e2dd2bd412bab2b8d3937e0812f675d
Josh Bassett <josh.bassett@gmail.com>
2018-09-13 17:06:49 +1000

    Add label to releases

--- src/components/releases/index.jsx
+++ src/components/releases/index.jsx
@@ -3,13 +3,18 @@ import {withPrefix} from 'gatsby'
 
 import styles from './styles.module.scss'
 
-const Release = ({release: {title, year, cover, url}}) => {
+const Release = ({release: {title, year, label, cover, url}}) => {
   return (
     <li>
-      <img alt='cover' className={styles.cover} src={withPrefix(cover)} />
-      <header>{title}</header>
-      <span className={styles.year}>{year}</span>
-      <a href={url}>Buy on Bandcamp</a>
+      <img alt='cover' src={withPrefix(cover)} />
+      <div>
+        <header>
+          <h4>{title}</h4>
+          <span className={styles.label}>{label}, </span>
+          <span className={styles.year}>{year}</span>
+        </header>
+        <a href={url}>Listen</a>
+      </div>
     </li>
   )
 }
--- src/pages/releases.jsx
+++ src/pages/releases.jsx
@@ -24,6 +24,7 @@ export const pageQuery = graphql`
           id
           title
           year
+          label
           cover
           url
         }
